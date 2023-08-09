c9c1275db6e49bf791d5bc818812218fd1bde816
Kayr0r404 <mahlarekarabo.702@gmail.com>
2023-08-09 22:45:37 +0200

    is upper

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def uppercase(input_str):
+    output_str = ""
+    
+    for char in input_str:
+        if 'a' <= char <= 'z':
+            output_str += chr(ord(char) - 32)
+        else:
+            output_str += char
