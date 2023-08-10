a6d82d6eb428089659669167dde686858480ceb1
Kayr0r404 <mahlarekarabo.702@gmail.com>
2023-08-10 08:54:21 +0200

    insert a node in a sorted list

---
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -0,0 +1,46 @@
+#include "lists.h"
+
+/**
+ * insert_node - insert a node in sorted list
+ * @head: input list
+ * @number: input number
+ * Return: return the new node, NULL otherwise
+*/
+listint_t *insert_node(listint_t **head, int number)
+{
+	listint_t *newNode, *curr, *fast;
+
+	if (!head)
+		return (NULL);
+
+	newNode = malloc(sizeof(listint_t));
+	if (!newNode)
+		return (NULL);
+
+	newNode->n = number;
+	curr = *head;
+	fast = (*head)->next;
+	if (curr->n > number)
+	{
+		newNode->next = curr;
+		return (newNode);
+	}
+
+	while (curr && fast)
+	{
+		if (curr->n < number && fast->n > number)
+		{
+			curr->next = newNode;
+			newNode->next = fast;
+			return (newNode);
+		}
+
+		curr = curr->next;
+		fast = fast->next;
+	}
+
+	fast->next = newNode;
+	newNode->next = NULL;
+
+	return (newNode);
+}
