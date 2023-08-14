f83353ca612fa508d342877755c049960ee608f2
Kayr0r404 <mahlarekarabo.702@gmail.com>
2023-08-14 12:18:08 +0200

    delete elementin a list

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -1,12 +1,7 @@
 #!/usr/bin/python3
 def delete_at(my_list=[], idx=0):
-    if (idx >= len(my_list) or idx < 0):
+    if (idx > len(my_list) or idx < 0):
         return (my_list)
     else:
-        newList = []
-        for i in range(len(my_list)):
-            if (i == idx):
-                continue
-            else:
-                newList.append(my_list[i])
-        return (newList)
+        del (my_list[idx])
+        return (my_list)
