34fa6cffc6ca172a43401abed05694678055b800
Kayr0r404 <mahlarekarabo.702@gmail.com>
2023-08-14 11:21:32 +0200

    tuple addition

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,24 +1,19 @@
-#!/usr/bin/python3
+#!/usr/nin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    if len(tuple_a) == 0:
-        a1 = 0
-        a2 = 0
-    elif len(tuple_a) == 1:
-        a1 = tuple_a[0]
-        a2 = 0
-    else:
-        a1 = tuple_a[0]
-        a2 = tuple_a[1]
-
-    if len(tuple_b) == 0:
-        b1 = 0
-        b2 = 0
-    elif len(tuple_b) == 1:
-        b1 = tuple_b[0]
-        b2 = 0
-    else:
-        b1 = tuple_b[0]
-        b2 = tuple_b[1]
-
-    newTuple = (a1 + b1, a2 + b2)
-    return (newTuple)
+    if (len(tuple_a) == 0 and len(tuple_b) == 0):
+        tuple = ()
+        return (tuple)
+    if (len(tuple_a) == 0):
+        a1, a0 = 0, 0
+    if (len(tuple_b) == 0):
+        b0, b1 = 0, 0
+    if (len(tuple_a) > 0 and len(tuple_a) < 2):
+        a0, a1 = tuple_a[0], 0
+    if (len(tuple_b) > 0 and len(tuple_b) < 2):
+        b0, b1 = tuple_b[0], 0
+    if (len(tuple_a) >= 2):
+        a0, a1 = tuple_a[0], tuple_a[1]
+    if (len(tuple_b) >= 2):
+        b0, b1 = tuple_b[0], tuple_b[1]
+    tuple = (a0 + b0, a1 + b1)
+    return (tuple)
