13ba3a77e31243f20b76c46ff474c6f266cf3036
Kayr0r404 <mahlarekarabo.702@gmail.com>
2023-08-16 19:20:52 +0200

    add unique elements in a list

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -2,7 +2,7 @@
 def uniq_add(my_list=[]):
     unique_integers = set()  # Using a set to store unique integers
     total_sum = 0
-    
+
     for num in my_list:
         if num not in unique_integers:
             total_sum += num
