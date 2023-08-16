202dfeb15cd42ecb63001b874fe1f17534240bfd
Kayr0r404 <mahlarekarabo.702@gmail.com>
2023-08-16 21:03:23 +0200

    find max in a dic

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    maxi, maxiKey = None, None
+
+    if (a_dictionary == None):
+        return None
+    for key, value in a_dictionary.items():
+        if maxi is None or value > maxi:
+            maxi = value
+            maxiKey = key
+    return maxiKey
