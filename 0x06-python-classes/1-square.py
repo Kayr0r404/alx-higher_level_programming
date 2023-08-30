2a6f3d7548a64e506f8ac7eab25773635a752326
Kayr0r404 <mahlarekarabo.702@gmail.com>
2023-08-30 18:53:56 +0200

    obj

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" empty square class """
+
+
+class Square:
+    """ empty square class """
+    def __init__(self, size):
+        """
+        Write a class Square that defines a square by: (based on 0-square.py)
+        args:
+        size (int): Private instance attribute: size
+                    Instantiation with size (no type/value verification)
+        """
+        self.__size = size
