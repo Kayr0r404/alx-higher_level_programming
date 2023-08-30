c4b9cc177aceb3ccae05dbfc7e5c00ad8634dbf9
Kayr0r404 <mahlarekarabo.702@gmail.com>
2023-08-30 18:19:29 +0200

    square claa

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,3 +1,10 @@
-class Square:
+#!/usr/bin/python3
+class Square :
+    '''
+    empty square class
+    '''
     def __init__(self):
+        '''
+        empty square class
+        '''
         pass
