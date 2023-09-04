c3907758bc1b7627d5b6ca0cb4525748ab14af0b
Kayr0r404 <mahlarekarabo.702@gmail.com>
2023-09-04 20:23:00 +0200

    empty class and getter and setter setup for rectangle

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -1,17 +1,35 @@
 #!/usr/python3
 
-""" empty square class """
+""" 
+contains 5 methods, including the constructor,
+two setters and two getters
+"""
 
 
 class Rectangle:
+    """
+    implemnets the rectange
+    """
     def __init__(self, width=0, height=0):
+        """
+        The constructor:
+        Args:
+        width: input int, that is the width of the rectangles
+        height: input int, the height of the rectangle
+        """
         self.width = width
         self.height = height
     @property
     def width(self):
+        """The getter for the width of the width
+        Return: the width value
+        """
         return (self.__width)
     @width.setter
     def width(self, value):
+        """
+        set the width value
+        """
         if not(isinstance(value, int)):
             raise TypeError("idth must be an integer")
         if (value < 0):
@@ -20,10 +38,16 @@ class Rectangle:
 
     @property
     def height(self):
+        """The getter for the Height of the width
+        Return: the height value
+        """
         return (self.__height)
 
     @width.setter
     def height(self, value):
+        """
+        set the height value
+        """
         if not(isinstance(value, int)):
             raise TypeError("idth must be an integer")
         if (value < 0):
