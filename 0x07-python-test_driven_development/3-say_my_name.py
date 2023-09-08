7026fa8c3b6fe86a237907703c090eae684b6990
Kayr0r404 <mahlarekarabo.702@gmail.com>
2023-09-08 20:25:30 +0200

    test

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -1,14 +1,21 @@
 #!/usr/bin/python3
 
 def say_my_name(first_name, last_name=""):
+    """
+     a function that prints My name is <first name> <last name>
+
+     first_name and last_name must be strings otherwise, raise a
+     TypeError exception with the message first_name must be a
+     string or last_name must be a strin
+    """
     if not(isinstance(first_name, str)):
         raise TypeError("first_name must be a string")
     if not(isinstance(last_name, str)):
         raise TypeError("last_name must be a string")
-    if (len(last_name) == 0):
-        print(f"My name is {first_name}")
-    else:
-        print(f"My name is {first_name} {last_name}")
+    # if (len(first_name) == 0 and last_name == ""):
+    #     print("")
+    # else:
+    print(f"My name is {first_name} {last_name}")
 
     if __name__ == "__main__":
         import doctest
