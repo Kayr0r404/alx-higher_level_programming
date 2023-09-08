fd40b7e84b920aa84cc5b37d5fbb1366c49f5b5b
Kayr0r404 <mahlarekarabo.702@gmail.com>
2023-09-08 21:10:59 +0200

    indentation

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -1,14 +1,23 @@
 #!/usr/bin/python3
 
+
 def text_indentation(text):
-    if not(isinstance(text, str)):
+    """
+     function prints a text with 2 new lines after each
+     of these characters: ., ? and :
+
+    """
+    if not (isinstance(text, str)):
         raise TypeError("text must be a string")
     new_text = ""
     for i in text:
         new_text += i
         if (i == "." or i == "?" or i == ":"):
             print(new_text.lstrip())
+            print()
             new_text = ""
+
+
 if __name__ == "__main__":
     import doctest
     doctest.testfile("tests/5-text_indentation.txt")
