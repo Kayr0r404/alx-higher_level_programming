c0eb2edaed8c4f62e67b83deb2bcc482c53a9eab
Kayr0r404 <mahlarekarabo.702@gmail.com>
2023-09-08 20:40:42 +0200

    print

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -1,21 +1,24 @@
 #!/usr/bin/python3
+"""Implementation"""
+
 
 def print_square(size):
     """
-    a function that prints a square with the character #
+    function  prints a square with the character #
     Arg:
     size: is the size of the aquare
     """
-    if not(isinstance(size, int)):
+    if not (isinstance(size, int)):
         raise TypeError("size must be an integer")
     if (size < 0):
         raise ValueError("size must be >= 0")
-    if (isinstance(size, float) and size < 0):
+    if (isinstance(size, (list, str, float))):
         raise TypeError("size must be an integer")
     for i in range(size):
         for j in range(size):
             print("#", end='')
         print()
+
 
 if __name__ == "__main__":
     import doctest
