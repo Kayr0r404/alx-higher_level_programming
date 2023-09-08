687f26c48f42a2c96581ab81265e8fc77912475a
Kayr0r404 <mahlarekarabo.702@gmail.com>
2023-09-08 20:30:14 +0200

    tests

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
+"""implementation"""
+
 
 def say_my_name(first_name, last_name=""):
     """
@@ -8,15 +10,16 @@ def say_my_name(first_name, last_name=""):
      TypeError exception with the message first_name must be a
      string or last_name must be a strin
     """
-    if not(isinstance(first_name, str)):
+    if not (isinstance(first_name, str)):
         raise TypeError("first_name must be a string")
-    if not(isinstance(last_name, str)):
+    if not (isinstance(last_name, str)):
         raise TypeError("last_name must be a string")
     # if (len(first_name) == 0 and last_name == ""):
     #     print("")
     # else:
     print(f"My name is {first_name} {last_name}")
 
-    if __name__ == "__main__":
-        import doctest
-        doctest.testfile("tests/3-say_my_name.txt")
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testfile("tests/3-say_my_name.txt")
