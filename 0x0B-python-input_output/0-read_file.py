83981e52b468e00e7324659a7bf8c4effd8eef0c
Kayr0r404 <mahlarekarabo.702@gmail.com>
2023-09-12 22:31:29 +0200

    encoding = uft-8

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -6,4 +6,4 @@ def read_file(filename=""):
     """reads file contents and ptint to the stdout"""
     with open(filename, 'r', encoding='utf-8') as file:
         contents = file.read()
-        print(contents.rstrip())
+        print(contents, end='')
