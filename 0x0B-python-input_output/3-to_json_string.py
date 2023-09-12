ca11d7d2b3c93489eb5c83f6d5d4db06ab4f1b27
Kayr0r404 <mahlarekarabo.702@gmail.com>
2023-09-13 00:20:31 +0200

    to json string

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+import json
+"""How to covert python objects into json string"""
+
+
+def to_json_string(my_obj):
+    """converts python objects into json string"""
+    return json.dumps(my_obj)
