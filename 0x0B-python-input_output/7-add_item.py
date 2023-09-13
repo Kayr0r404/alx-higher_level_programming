e9eeea0020c6978ce6ae53f0713d06f1339ae6cb
Kayr0r404 <mahlarekarabo.702@gmail.com>
2023-09-13 16:34:33 +0200

    scrip

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -3,11 +3,13 @@ import sys
 save = __import__("5-save_to_json_file").save_to_json_file
 load = __import__("6-load_from_json_file").load_from_json_file
 
-del sys.argv[0]
+if __name__ == "__name__":
 
-try:
-    data = load("add_item.json")
-    data.extend(sys.argv)
-    save(data, "add_item.json")
-except FileNotFoundError:
-    save(sys.argv, "add_item.json")
+    del sys.argv[0]
+
+    try:
+        data = load("add_item.json")
+        data.extend(sys.argv)
+        save(data, "add_item.json")
+    except FileNotFoundError:
+        save(sys.argv, "add_item.json")
