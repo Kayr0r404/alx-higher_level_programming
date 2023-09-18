194c32acc189eff221fc1d7eedfa61eec01c9473
Kayr0r404 <mahlarekarabo.702@gmail.com>
2023-09-18 23:30:12 +0200

    <= 0 added as validator in the gtter  methods

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -22,7 +22,7 @@ class Rectangle(Base):
         """setter method for width"""
         if not isinstance(width, int):
             raise TypeError("width must be an integer")
-        if width < 0:
+        if width <= 0:
             raise ValueError("width must be > 0")
         self.__width = width
 
@@ -36,7 +36,7 @@ class Rectangle(Base):
         """setter method for height"""
         if not isinstance(height, int):
             raise TypeError("height must be an integer")
-        if height < 0:
+        if height <= 0:
             raise ValueError("height must be > 0")
         self.__height = height
 
