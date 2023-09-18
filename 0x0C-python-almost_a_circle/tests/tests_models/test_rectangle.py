64c52bed3027a1c0493d64b77bfae5ff7a2d2e06
Kayr0r404 <mahlarekarabo.702@gmail.com>
2023-09-18 22:52:45 +0200

    rec

--- 0x0C-python-almost_a_circle/tests/tests_models/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/tests_models/test_rectangle.py
@@ -1,8 +1,17 @@
 import unittest
+from models.base import Base
 from models.rectangle import Rectangle
 
-class TestRectangle(unittest.TestCase):
-    pass
+
+class TestRectangle_instantiation(unittest.TestCase):
+    """Unittests for testing instantiation of the Rectangle class."""
+
+    def test_rectangle_is_base(self):
+        self.assertIsInstance(Rectangle(10, 2), Base)
+
+    def test_no_args(self):
+        with self.assertRaises(TypeError):
+            Rectangle()
 
 if __name__ == "__main__":
     unittest.main()
