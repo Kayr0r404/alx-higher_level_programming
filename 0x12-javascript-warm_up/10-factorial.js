7c86e7fa0c2452a7d5bee9d5804c3b0ae25baa65
Kayr0r404 <mahlarekarabo.702@gmail.com>
2023-11-14 01:03:23 +0200

    fact!

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
 
 function factorial (a) {
-  if (a === 0 && !isNaN(a)) { console.log(1); } else if (!isNaN(a)) {
+  if (a === 0) { console.log(1); } else {
     let fact = 1;
     for (let i = a; i > 0; i--) { fact *= i; }
     console.log(fact);
-  } else { console.log('NaN'); }
+  }
 }
 
 factorial(parseInt(process.argv[2]));
