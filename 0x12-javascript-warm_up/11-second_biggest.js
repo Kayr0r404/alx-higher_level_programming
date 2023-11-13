84bddafbec62b6c62181e1a43fbd97f96688ba61
Kayr0r404 <mahlarekarabo.702@gmail.com>
2023-11-14 01:46:02 +0200

    secod max

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+
+if (process.argv.length <= 3) { console.log(1); } else {
+  const numbers = process.argv.slice(2);
+  const max = Math.max(...numbers);
+  let secondMax = process.argv[2];
+
+  for (let i = 0; i < process.argv.length; i++) {
+    if (parseInt(process.argv[i]) === max) { continue; } else {
+      if (parseInt(process.argv[i]) > secondMax) {
+        secondMax = parseInt(process.argv[i]);
+      }
+    }
+  }
+
+  console.log(secondMax);
+}
