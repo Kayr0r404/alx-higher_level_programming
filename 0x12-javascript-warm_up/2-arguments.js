db53b2f5b9e3e4c0b53e132a9dd43a8c1e873ba9
Kayr0r404 <mahlarekarabo.702@gmail.com>
2023-11-13 22:16:25 +0200

    cmd-line args

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 
-if (process.argv.length > 2) {
+if (process.argv.length > 3) {
   console.log('Arguments found');
-} else if (process.arv.length === 2) { console.log('Argument found'); } else {
+} else if (process.argv.length === 3) { console.log('Argument found'); } else {
   console.log('No argument');
 }
