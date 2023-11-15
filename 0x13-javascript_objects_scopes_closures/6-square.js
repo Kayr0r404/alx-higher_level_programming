cb9e667ea271436bc62bd517611b576d5c49c474
Kayr0r404 <mahlarekarabo.702@gmail.com>
2023-11-15 20:34:46 +0200

    print char

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,21 @@
+#!/usr/bin/node
+
+const Rectangle = require('./4-rectangle');
+
+module.exports = class Square extends Rectangle {
+  constructor (size) {
+    super(size, size);
+  }
+
+  charPrint (c) {
+    if (!c) { c = 'X'; }
+
+    for (let row = 0; row < this.height; row++) {
+      let line = '';
+      for (let col = 0; col < this.width; col++) {
+        line += c;
+      }
+      console.log(line);
+    }
+  }
+};
