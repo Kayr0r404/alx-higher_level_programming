9d01d810a771569535b5b681d079c5aa6e67b997
Kayr0r404 <mahlarekarabo.702@gmail.com>
2023-11-15 16:39:23 +0200

    rec

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -2,9 +2,7 @@
 
 module.exports = class Rectangle {
   constructor (w, h) {
-    if ((w === 0 || w < 0) || (h === 0 || h < 0)) {
-      new Object();
-    } else if (w && h) {
+    if (w > 0 && h > 0) {
       this.width = w;
       this.height = h;
     }
