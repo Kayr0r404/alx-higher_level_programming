bcd4aebaa132e1e1a1c131ae191cadf427090a8f
Kayr0r404 <mahlarekarabo.702@gmail.com>
2023-11-20 00:55:55 +0200

    should not be executed whenn imported

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -6,12 +6,13 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 import sys
 
-usr, passwd, mydb = sys.argv[1], sys.argv[2], sys.argv[3]
-engine = create_engine("mysql+mysqldb://{}:{}@localhost:3306/{}".format(
-    usr, passwd, mydb))
-session = sessionmaker(bind=engine)
-session = session()
+if __name__ == '__main__':
+    usr, passwd, mydb = sys.argv[1], sys.argv[2], sys.argv[3]
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost:3306/{}".format(
+        usr, passwd, mydb))
+    session = sessionmaker(bind=engine)
+    session = session()
 
-states = session.query(State).all()
-for state in states:
-    print(str(state.id) + ": ", state.name)
+    states = session.query(State).all()
+    for state in states:
+        print(str(state.id) + ": ", state.name)
