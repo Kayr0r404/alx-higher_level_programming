0aa47a5447c0e18b8aad259fb1ce1be81bea9ce0
Kayr0r404 <mahlarekarabo.702@gmail.com>
2023-11-19 13:31:11 +0200

    multiple rows

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,9 +1,9 @@
 -- create table and add multiple rows
 
-USE hbtn_0c_0;
+-- USE hbtn_0c_0;
 
 CREATE TABLE IF NOT EXISTS second_table (
-    id INT AUTO_INCREMENT,
+    id INT,
     name VARCHAR(256),
     score INT
 );
