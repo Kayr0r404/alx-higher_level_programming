01bf28196b60c7fd6038286201f7bb5be69a7b92
Kayr0r404 <mahlarekarabo.702@gmail.com>
2023-11-20 05:55:38 +0200

    find

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -7,18 +7,18 @@ from sqlalchemy.orm import sessionmaker
 import sys
 
 if __name__ == '__main__':
-
     usr, passwd, mydb = sys.argv[1], sys.argv[2], sys.argv[3]
     engine = create_engine("mysql+mysqldb://{}:{}@localhost:3306/{}".format(
         usr, passwd, mydb))
-
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
-
     i, n = State.id, State.name
     query = session.query(State).filter(n == sys.argv[4]).order_by(i).all()
-
-    for state in query:
-        print("{}".format(state.id))
+    
+    if  query:
+        for state in query:
+            print("{}".format(state.id))
+    else:
+        print("Not Found")
     session.close()
