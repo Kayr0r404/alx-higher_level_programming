dc5b59ced06a5d4399c2824d3bf3b33fe16877a4
Karabo Mahlare <123119832+Kayr0r404@users.noreply.github.com>
2023-12-03 16:57:09 +0200

    Update 0-body_size.sh

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,23 +1,3 @@
 #!/bin/bash
-# sends a request to that URL, and displays the size of the body of the response
-
-# Check if URL argument is provided
-if [ -z "$1" ]; then
-    echo "Usage: $0 <URL>"
-    exit 1
-fi
-
-url=$1
-
-# Use curl to send a request and retrieve the size of the response body
-response_size=$(curl -sI "$url" | grep -i "content-length" | awk '{print $2}' | tr -d '\r\n')
-
-# Check if Content-Length header is present in the response
-if [ -z "$response_size" ]; then
-    echo "Content-Length header not found in the response."
-    exit 1
-fi
-
-# echo "Size of the response body: ${response_size} bytes"
-
-echo "$response_size"
+# takes in a URL, sends a request to that URL, and displays the size of the body of the response
+curl -s "$1" | wc -c
