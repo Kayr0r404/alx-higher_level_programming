9410c8eeee756f7a509f3709e2ec6226d4cdc570
Kayr0r404 <mahlarekarabo.702@gmail.com>
2024-01-04 18:53:24 +0200

    write to a file

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -2,21 +2,21 @@
 
 const fs = require('fs');
 
-function writeToFile(filePath, content) {
-    fs.writeFile(filePath, content, 'utf-8', (err) => {
-        if (err) {
-            console.error(`Error occurred while writing to ${filePath}: ${err.message}`);
-        } else {
-            console.log(`Content written to ${filePath}`);
-        }
-    });
+function writeToFile (filePath, content) {
+  fs.writeFile(filePath, content + '\n', 'utf-8', (err) => {
+    if (err) {
+      console.error(`Error occurred while writing to ${filePath}: ${err.message}`);
+    } else {
+      console.log(`Content written to ${filePath}`);
+    }
+  });
 }
 
 // Check if the file path and content are provided as command-line arguments
 if (process.argv.length !== 4) {
-    console.error('Usage: node script.js <file_path> <content>');
+  console.error('Usage: node script.js <file_path> <content>');
 } else {
-    const filePath = process.argv[2];
-    const content = process.argv[3];
-    writeToFile(filePath, content);
+  const filePath = process.argv[2];
+  const content = process.argv[3];
+  writeToFile(filePath, content);
 }
