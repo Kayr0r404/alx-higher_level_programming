172e52d2de9fefc59ca273de89f2a7c672ac0210
Kayr0r404 <mahlarekarabo.702@gmail.com>
2024-01-04 21:46:59 +0200

    complete proj

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,22 +1,5 @@
 #!/usr/bin/node
-
 const fs = require('fs');
-
-function writeToFile (filePath, content) {
-  fs.writeFile(filePath, content + '\n', 'utf-8', (err) => {
-    if (err) {
-      console.error(`Error occurred while writing to ${filePath}: ${err.message}`);
-    } else {
-      console.log(`Content written to ${filePath}`);
-    }
-  });
-}
-
-// Check if the file path and content are provided as command-line arguments
-if (process.argv.length !== 4) {
-  console.error('Usage: node script.js <file_path> <content>');
-} else {
-  const filePath = process.argv[2];
-  const content = process.argv[3];
-  writeToFile(filePath, content);
-}
+fs.writeFile(process.argv[2], process.argv[3], error => {
+  if (error) console.log(error);
+});
